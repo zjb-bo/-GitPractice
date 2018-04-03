a1574bd88966eb65c0c91a0b876b2502f2398723
Zheng jiabo <zhengjiabo_ext@smartisan.com>
2018-04-03 19:19:10 +0800

    log

--- app/src/main/java/com/example/zjb/myapplication/BugActivity.java
+++ app/src/main/java/com/example/zjb/myapplication/BugActivity.java
@@ -1,10 +1,19 @@
 package com.example.zjb.myapplication;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by ZJB on 2018/4/3.
  */
 
 public class BugActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("bug","relove bug");
+    }
 }
