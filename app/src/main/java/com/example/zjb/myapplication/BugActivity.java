04bbc474ff29cd9c014173523fe7c0af346e5b3d
Zheng jiabo <zhengjiabo_ext@smartisan.com>
2018-04-03 19:16:01 +0800

    resolve bug

---
+++ app/src/main/java/com/example/zjb/myapplication/BugActivity.java
@@ -0,0 +1,10 @@
+package com.example.zjb.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZJB on 2018/4/3.
+ */
+
+public class BugActivity extends AppCompatActivity{
+}
